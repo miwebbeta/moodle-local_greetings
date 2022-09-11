3ccf47ff4523fe87968e82f97e1b469ceb9dc68d
esther.ballester <esther.balles-ext@havasit.com>
2022-09-11 19:42:03 +0200

    Display a localised greeting to user

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
  
 
 require_once('../../config.php');
+require_once($CFG->dirroot. '/local/greetings/lib.php');
 $context = context_system::instance();
 $PAGE->set_context($context);
 $PAGE->set_url(new moodle_url('/local/greetings/index.php'));
@@ -28,9 +29,9 @@ $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 
 echo $OUTPUT->header();
 if (isloggedin()) {
-    echo '<h2>Greetings, ' . fullname($USER) . '</h2>';
+    echo local_greetings_get_greeting($USER);
 } else {
-    echo '<h2>Greetings, user</h2>';
+    echo get_string('greetinguser', 'local_greetings');
 }
 
 echo $OUTPUT->footer();
---
+++ lib.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+function local_greetings_get_greeting($user) {
+    if ($user == null) {
+        return get_string('greetinguser', 'local_greetings');
+    }
+
+    $country = $user->country;
+
+    switch ($country) {
+        case 'ES':
+            $langstr = 'greetinguseres';
+            break;
+        default:
+            $langstr = 'greetingloggedinuser';
+            break;
+    }
+
+    return get_string($langstr, 'local_greetings', fullname($user));
+}
