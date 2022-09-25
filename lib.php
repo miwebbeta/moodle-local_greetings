4a6ea3c2d7b91f847e9c3a7623a689b2af294937
esther.ballester <esther.balles-ext@havasit.com>
2022-09-25 18:27:44 +0200

    add .

--- lib.php
+++ lib.php
@@ -2,8 +2,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 /**
- * Get lang string
- * @param USER record mdl_user user logged
+ * Get a localised greeting message for a user
+ *
+ * @param \stdClass $user
+ * @return string
  */
 function local_greetings_get_greeting($user) {
     if ($user == null) {
@@ -13,8 +15,17 @@ function local_greetings_get_greeting($user) {
     $country = $user->country;
 
     switch ($country) {
+        case 'AU':
+            $langstr = 'greetinguserau';
+            break;
         case 'ES':
             $langstr = 'greetinguseres';
+            break;
+        case 'FJ':
+            $langstr = 'greetinguserfj';
+            break;
+        case 'NZ':
+            $langstr = 'greetingusernz';
             break;
         default:
             $langstr = 'greetingloggedinuser';
@@ -23,7 +34,6 @@ function local_greetings_get_greeting($user) {
 
     return get_string($langstr, 'local_greetings', fullname($user));
 }
-
 /**
  * Insert a link to index.php on the site front page navigation menu.
  *
@@ -31,25 +41,40 @@ function local_greetings_get_greeting($user) {
  */
 function local_greetings_extend_navigation_frontpage(navigation_node $frontpage)
 {
-    $frontpage->add(
-        get_string('pluginname', 'local_greetings'),
-        new moodle_url('/local/greetings/index.php'),
-        navigation_node::TYPE_CUSTOM,
-        null,
-        nul,
-        new pix_icon('i/edit', '')
-    );
+    require_login();
+    if (isloggedin() && !isguestuser()) {
+        
+
+        $frontpage->add(
+            get_string('pluginname', 'local_greetings'),
+            new moodle_url('/local/greetings/index.php'),
+            navigation_node::TYPE_CUSTOM,
+            null,
+            null,
+            new pix_icon('t/message', '')
+        );
     
-
+    }
 }
+/**
+ * Add link to index.php into navigation drawer.
+ *
+ * @param global_navigation $root Node representing the global navigation tree.
+ */
+function local_greetings_extend_navigation(global_navigation $root) {
+    require_login();
+    if (isloggedin() && !isguestuser()) {
+        $node = navigation_node::create(
+            get_string('pluginname', 'local_greetings'),
+            new moodle_url('/local/greetings/index.php'),
+            navigation_node::TYPE_CUSTOM,
+            null,
+            null,
+        new pix_icon('t/message', '')
+        );
 
-function local_greetings_extend_navigation(global_navigation $root)
-{
-    $node = navigation_node::create(
-        get_string('pluginname', 'local_greetings'),
-        new moodle_url('/local/greetings/index.php')
-    );
+        $node->showinflatnavigation = true;
 
-    $node->showinflatnavigation = true;
-    $root->add_node($node);
+        $root->add_node($node);
+    }
 }
